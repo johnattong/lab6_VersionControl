c6a3d5dcc185990ced085f0faca395f863226d32
tran-justin <tranjustin@ufl.edu>
2023-10-25 18:15:20 -0400

    decoded function added

--- pythonProject/lab6.py
+++ pythonProject/lab6.py
@@ -5,6 +5,19 @@ def encode(password):
         encoded_password += str(i)
     return encoded_password
 
+def decode(encoded_password):
+    result_list = []
+    result_str = ""
+    for i in encoded_password:
+        i = int(i)
+        result = i - 3
+        if result >= 10:
+            result -= 10
+        result_list.append(result)
+        result = 0
+    for j in result_list:
+        result_str += str(j)
+    return result_str
 
 def main():
     choice = 0
@@ -21,6 +34,8 @@ def main():
             print('Your password has been encoded and stored!\n')
         elif choice == 2:
             print(f'The encoded password is {encoded_password}, and the original password is {password}.\n')
+            decoded_password = decode(encoded_password)
+            print("This is the decoded password again", decoded_password)
         elif choice == 3:
             break
 
